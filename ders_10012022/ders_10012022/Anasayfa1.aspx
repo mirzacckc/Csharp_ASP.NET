b462515be93f8f923bdb5a656cd9a5d0e1c17a1f
mirzacckc <cicekcimirzacan@gmail.com>
2022-04-17 23:43:41 +0300

    ders_10012022 dosyalari eklendi

---
+++ ders_10012022/ders_10012022/Anasayfa1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Anasayfa1.aspx.cs" Inherits="ders_10012022.Anasayfa1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="Label1" runat="server" Text="Vize Notunu Giriniz: " Width="140px"></asp:Label>
+
+            <asp:TextBox ID="txt_vize" runat="server"></asp:TextBox>
+
+            <br /><br />
+
+            <asp:Label ID="Label2" runat="server" Text="Final Notunu Giriniz: " Width="140px"></asp:Label>
+
+            <asp:TextBox ID="txt_final" runat="server"></asp:TextBox>
+
+            <br /><br />
+
+            <asp:Button ID="Button1" runat="server" Text="Ortalama Hesapla" OnClick="Button1_Click" BorderColor="Yellow" BackColor="SkyBlue" ForeColor="Red" />
+
+            <br /><br />
+
+            <asp:Label ID="lbl_sonuc" runat="server" Text=""></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ders_10012022/ders_10012022/Anasayfa1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ders_10012022
+{
+    public partial class Anasayfa1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int vize, final;
+            vize = Convert.ToInt32(txt_vize.Text);
+            final = Convert.ToInt32(txt_final.Text);
+
+            double ort = vize * 0.6 + final * 0.4;
+            string harfNotu;
+
+            if(ort <= 100 && ort >= 90)
+            {
+                harfNotu = "AA";
+
+            }
+            else if (ort < 90 && ort >= 80)
+            {
+                harfNotu = "BA";
+            }
+            else if (ort < 80 && ort >= 70)
+            {
+                harfNotu = "BB";
+            }
+            else 
+            {
+                harfNotu = "FF";
+            }
+            
+            
+            lbl_sonuc.Text = "Ortalamanız: " + ort;
+            //lbl_sonuc.Text = "Ortalamanız: " + ort + " " + "Harf Notunuz: " + harfNotu;
+            //lbl_sonuc.Text = lbl_sonuc.Text + " Harf Notu" + harfNotu;
+            lbl_sonuc.Text += " Harf Notunuz: " + harfNotu;
+
+            //vize += 10
+            // vize = vize + 10 ikisi aynı durum
+
+            //final -= 5
+            //final = final -5 ikisi aynı durum
+
+            double sozlu = vize * 2 - final * 0.5;
+            if (sozlu < 0)
+            {
+                sozlu *= -1;
+            }
+            if (sozlu > 100)
+            {
+                sozlu = 100;
+            }
+
+            lbl_sonuc.Text += " Sozlu Notunuz: " + sozlu;
+        }   
+    }
+}
