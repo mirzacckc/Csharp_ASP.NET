02c9f70de262fd196faa4341840f0873632ac189
mirzacckc <cicekcimirzacan@gmail.com>
2022-04-17 23:15:33 +0300

    ders_07012022 dosyalari eklendi

---
+++ ders_07012022/ders_07012022/Anasayfa.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Anasayfa.aspx.cs" Inherits="ders_07012022.Anasayfa" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>My First Project</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="Label1" runat="server" Text="Adınızı Giriniz: " Width="250px"></asp:Label>
+                        
+            <asp:TextBox ID="txt_ad" runat="server"></asp:TextBox>
+
+            <br /><br />
+
+            <asp:Label ID="Label2" runat="server" Text="Cinsiyet Bilgisi(Kadın: k, Erkek: e)" Width="250px"></asp:Label>
+
+            <asp:TextBox ID="txt_cinsiyet" runat="server"></asp:TextBox>
+
+            <br /><br />
+
+            <asp:Button ID="Button1" runat="server" Text="Hoşgeldiniz" OnClick="Button1_Click" />
+            <br /><br />
+
+            <asp:Label ID="Label3" runat="server" Text="Yaşınızı Giriniz" Width="250px"></asp:Label>
+
+            <asp:TextBox ID="txt_yas" runat="server"></asp:TextBox>
+            <br /><br />
+
+            <asp:Button ID="Button2" runat="server" Text="Yaş Kontrol" OnClick="Button2_Click" />
+            <br /><br />
+
+            <asp:Label ID="lbl_sonuc" runat="server" Text=""></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ders_07012022/ders_07012022/Anasayfa.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ders_07012022
+{
+    public partial class Anasayfa : System.Web.UI.Page
+    {
+        //string kullaniciAdi = "Mirza";
+        //char cinsiyetBilgisi = 'e';
+        //int hisettiginYas;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //hisettiginYas = 105;
+            //double benzinFiyati = 13.50;
+            //bool hayatPahali = true;     //bool ve Boolean aynı işlevi görür
+
+            //Label1.Text = kullaniciAdi;
+            //TextBox1.Text = Convert.ToString(hisettiginYas);
+            ////TextBox2.Text = "" + benzinFiyati;
+
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string isim = txt_ad.Text;
+            char cinsiyet = Convert.ToChar(txt_cinsiyet.Text);
+
+            if (cinsiyet == 'k')
+            {
+                lbl_sonuc.Text = "Hoşgeldiniz " + isim + " Hanım :D";
+            }
+            if (cinsiyet == 'e')
+            {
+                lbl_sonuc.Text = "Hoşgeldiniz " + isim + " Bey :D";
+
+            }
+
+            //string textboxOku = TextBox2.Text;
+
+            ////Label1.Text = textboxOku + " " + kullaniciAdi + " " + cinsiyetBilgisi + " " + hisettiginYas;
+
+            //if (cinsiyetBilgisi == 'k')
+            //{
+            //    Label1.Text = "Hoşgeldiniz " + textboxOku + " Hanım";
+            //}
+            //if (cinsiyetBilgisi == 'e')
+            //{
+            //    Label1.Text = "Hoşgeldiniz " + textboxOku + " Bey";
+            //}
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int yas = Convert.ToInt32(txt_yas.Text);
+
+            if (yas > 14)
+            {
+                lbl_sonuc.Text = "Welcome to Chamber of Secret";
+            }
+            if (yas <= 14)
+            {
+                lbl_sonuc.Text = "Ruh emiciler tarafından engellendin Heri Pıttır";
+            }
+        }
+    }
+}
